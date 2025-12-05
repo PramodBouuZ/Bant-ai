d27ca4411c8d4c91964bdbfd12fc20e74bde1fec
Pramod <info.bouuz@gmail.com>
2025-12-05 19:03:34 +0530

    Add imports for supabase and components

--- components/admin/EnquiryManagementTable.tsx
+++ components/admin/EnquiryManagementTable.tsx
@@ -1,3 +1,8 @@
+import React, { useEffect, useState } from "react";
+import { supabase } from "../../../services/supabase";
+import { Enquiry } from "../../../types";
+import LoadingSpinner from "../../LoadingSpinner";
+import Modal from "../../Modal";
 import type { Enquiry } from '../../types';
 
 const EnquiryManagementTable: React.FC = () => {
